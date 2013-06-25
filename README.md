1977895ebe274cf1aa96aa7445df0d1cd88ed0e8
Suhwan Hwang <entercritical@gmail.com>
2013-06-25 18:13:32 +0900

    add jar

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ SwanFlickrUploader
 ==================
 
 Very Simple CommandLine Flickr Uploader for personal use
+
+
+1. Get Flickr API Key
+
+2. Edit src/com/swan/swanflickruploader
+
+	apiKey = your api key
+	secret = your secret
+	nsid = your nsid
+
+3. Run 
+	java -jar SwanFlickrUploader [Target Directory]
--- src/com/swan/swanflickruploader/Upload.java
+++ src/com/swan/swanflickruploader/Upload.java
@@ -31,12 +31,13 @@ public class Upload {
     private Flickr flickr;
 
     private AuthStore authStore;
-    
+
     private void authorize() throws IOException, SAXException, FlickrException {
         AuthInterface authInterface = flickr.getAuthInterface();
         Token accessToken = authInterface.getRequestToken();
 
-        String url = authInterface.getAuthorizationUrl(accessToken, Permission.DELETE);
+        String url = authInterface.getAuthorizationUrl(accessToken,
+                Permission.DELETE);
         System.out.println("Follow this URL to authorise yourself on Flickr");
         System.out.println(url);
         System.out.println("Paste in the token it gives you:");
@@ -44,17 +45,19 @@ public class Upload {
 
         String tokenKey = new Scanner(System.in).nextLine();
 
-        Token requestToken = authInterface.getAccessToken(accessToken, new Verifier(tokenKey));
+        Token requestToken = authInterface.getAccessToken(accessToken,
+                new Verifier(tokenKey));
 
         Auth auth = authInterface.checkToken(requestToken);
         RequestContext.getRequestContext().setAuth(auth);
         this.authStore.store(auth);
-        System.out.println("Thanks.  You probably will not have to do this every time.");
+        System.out
+                .println("Thanks.  You probably will not have to do this every time.");
     }
 
-    public Upload (File authsDir) throws FlickrException, IOException{
+    public Upload(File authsDir) throws FlickrException, IOException {
         Properties properties;
-        InputStream in = null;        
+        InputStream in = null;
         try {
             in = Upload.class.getResourceAsStream("setup.properties");
             properties = new Properties();
@@ -62,17 +65,20 @@ public class Upload {
         } finally {
             IOUtilities.close(in);
         }
-        
-        flickr = new Flickr(properties.getProperty("apiKey"), properties.getProperty("secret"), new REST());
+
+        flickr = new Flickr(properties.getProperty("apiKey"),
+                properties.getProperty("secret"), new REST());
         this.nsid = properties.getProperty("nsid");
 
         if (authsDir != null) {
             this.authStore = new FileAuthStore(authsDir);
         }
     }
-    public void upload(File directory) throws IOException, SAXException, FlickrException {
+
+    public void upload(File directory) throws IOException, SAXException,
+            FlickrException {
         RequestContext rc = RequestContext.getRequestContext();
-        
+
         if (this.authStore != null) {
             Auth auth = this.authStore.retrieve(this.nsid);
             if (auth == null) {
@@ -81,54 +87,56 @@ public class Upload {
                 rc.setAuth(auth);
             }
         }
-        
+
         // 일단 사용자가 지정한 directory 밑에는 디렉토리들 존재하고
         // 디렉토리 명을 세트명으로 하고 *.jpg 등 이미지 파일을 올리는 경우만 고려한다.
         File[] contents = directory.listFiles();
-        
+
         Uploader uploader = flickr.getUploader();
         PhotosInterface pint = flickr.getPhotosInterface();
         InputStream in = null;
-        
+
         for (int i = 0; i < contents.length; i++) {
             if (contents[i].isDirectory()) {
                 System.out.println("<<" + contents[i].getName() + ">>");
                 File[] list = contents[i].listFiles(new ImageFilenameFilter());
-                
+
                 for (int j = 0; j < list.length; j++) {
                     if (list[j].isFile()) {
                         System.out.println("Uploading : " + list[j].getName());
-                        
+
                         try {
                             in = new FileInputStream(list[j]);
                             UploadMetaData metaData = new UploadMetaData();
                             metaData.setPublicFlag(false);
                             metaData.setHidden(true);
-                            
+
                             // check correct handling of escaped value
                             metaData.setTitle(list[j].getName());
                             String photoId = uploader.upload(in, metaData);
-                            
-                            //assertNotNull(photoId);
-                            //pint.delete(photoId);
+
+                            // assertNotNull(photoId);
+                            // pint.delete(photoId);
                         } finally {
                             IOUtilities.close(in);
                         }
-                        
+
                     }
                 }
-             }
+            }
         }
     }
-    
+
     public static void main(String[] args) throws Exception {
-//      if (args.length < 4) {
-//          System.out.println("Usage: java " + Backup.class.getName() + " api_key nsid shared_secret output_dir");
-//          System.exit(1);
-//      }
-      
-        Upload upload = new Upload(new File(System.getProperty("user.home") + File.separatorChar + ".flickrAuth"));
-        
-        upload.upload(new File("/Users/janush/Documents/tmp/2005"));
+        if (args.length < 1) {
+            System.out
+                    .println("Usage: java -jar SwanFlickrUploader target_dir");
+            System.exit(1);
+        }
+
+        Upload upload = new Upload(new File(System.getProperty("user.home")
+                + File.separatorChar + ".flickrAuth"));
+
+        upload.upload(new File(args[0]));
     }
 }
